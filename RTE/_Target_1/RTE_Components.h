75450a6d164334972bc45b8f5ce536dd8ef0784f
Kevin David <kevindavidvenyk@gmail.com>
2026-05-13 18:39:23 +0300

    Add project sources, drivers, RTOS & Tiva headers
    Initial commit adding application skeleton and platform headers. Introduces App-level utilities (basic_io, consoleprint, main, retarget), TI Tiva device headers (hw_memmap, hw_sysctl, hw_types), startup/system files, FreeRTOS config and RTE project files, driver implementations for GPIO/LED/button, task and FSM placeholders, and common type/pin/event headers. Includes third-party/license headers from FreeRTOS, Code Red, and TI as provided by vendor libraries.

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,40 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SmartParkingGate_RTOS' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "TM4C123.h"
+
+/* ARM.FreeRTOS::RTOS:Config:CMSIS RTOS2:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Coroutines:11.2.0 */
+#define RTE_RTOS_FreeRTOS_COROUTINE     /* RTOS FreeRTOS Co-routines */
+/* ARM.FreeRTOS::RTOS:Event Groups:11.2.0 */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:11.2.0 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Message Buffer:11.2.0 */
+#define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
+/* ARM.FreeRTOS::RTOS:Stream Buffer:11.2.0 */
+#define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
+/* ARM.FreeRTOS::RTOS:Timers:11.2.0 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* ARM::CMSIS:RTOS2:FreeRTOS:Cortex-M:11.2.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+
+
+#endif /* RTE_COMPONENTS_H */
